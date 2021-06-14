784211057e06f6b926e90ec421a05632d7565348
Steve Giordano <stephenagiordano@gmail.com>
2021-06-13 22:46:27 -0400

    CORS?

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const path = require("path")
 const express = require("express")
 const app = express()
+const cors = require('cors')
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors())
 
 const port = process.env.PORT || 5000
 
