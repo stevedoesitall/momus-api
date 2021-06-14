5a237b1881287502f36508f2fbc54b99ad5d24bd
Steve Giordano <stephenagiordano@gmail.com>
2021-06-13 21:10:20 -0400

    API v1

---
+++ routes/deities.js
@@ -0,0 +1,61 @@
+const express = require("express")
+const router = express.Router()
+
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+
+router.get("/", async (req, res) => {
+	// const allowedParams = [ "domain", "name", "region" ]
+	const queryParams = req.query
+	const hasParams = Object.keys(queryParams).length
+
+	try {
+		let results
+
+		if (hasParams) {
+			const domain = queryParams.domain.toLowerCase()
+			results = await prisma.$queryRaw(`SELECT * FROM deities WHERE '${domain}' = ANY(domain)`)
+		} else {
+			results = await prisma.deities.findMany()
+		}
+
+		if (!results.length) {
+			return res.status(204)
+		}
+
+		res.json(results)
+
+	} catch(err) {
+		res.json(err)
+
+	} finally {
+		prisma.$disconnect()
+	}
+})
+
+router.get("/:id", async (req, res) => {
+	try {
+		let { id } = req.params
+		id = parseInt(id)
+		
+		const results = await prisma.deities.findUnique({
+			where: {
+				id: id
+			}
+		})
+
+		if (!results.length) {
+			return res.status(204)
+		}
+
+		res.json(results)
+
+	} catch(err) {
+		res.json(err)
+
+	} finally {
+		prisma.$disconnect()
+	}
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,28 @@
+const path = require("path")
+const express = require("express")
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+const port = process.env.PORT || 5000
+
+const deitiesRoute = require("./routes/deities")
+
+app.use(express.static("public"))
+app.use("/deities", deitiesRoute)
+
+app.get("*", (req, res) => {
+    res.send({
+        message: "Bad request",
+        status: 404
+    })
+})
+
+app.listen(port, err => {
+	if (err) {
+		console.log("Error starting server")
+	} else {
+		console.log(`Server running on port ${port}`)
+	}
+})
